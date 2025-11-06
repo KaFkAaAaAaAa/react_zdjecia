24290a17c2ffb968e3e885ae32d6cc2389f7af93
student <piwo.piwo@gmail.com>
2025-11-06 15:04:12 +0100

    piwny czwartek2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-
+import zdjecie from './assets/zdj1.jpg'
+import zdjecie2 from './assets/zd2.jpg'
+import zdjecie3 from './assets/zdj3.jpg'
 import './App.css'
 import {useState} from "react";
 
@@ -13,8 +15,8 @@ function App() {
         <label htmlFor="school">Szkoła</label>
         <input type="checkbox" id="home" onChange={ (e) => {setHome(e.target.checked)} }></input>
         <label htmlFor="school">Dom</label>
-        <p>{home? <>DOM TAK</> : <>DOM NIE</>}</p>
-        <p>{school? <>SZKOŁA TAK</> : <>SZKOŁA NIE</>}</p>
+        <p>{home? <><img src={zdjecie} alt="1" style={{width: 100}}/></> : <>DOM NIE</>}</p>
+        <p>{school? <><img src={zdjecie2} alt="2" style={{width: 100}}/><img src={zdjecie3} alt="3" style={{width: 100}}/></> : <>SZKOŁA NIE</>}</p>
     </div>
   )
 }
